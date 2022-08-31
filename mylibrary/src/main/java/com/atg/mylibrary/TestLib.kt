acd3f4068cdb7e5207aa09f849ffb8c2cb037abc
ThongNguyen <dinhthong.nguyen@alttekglobal.com>
2022-08-31 11:08:51 +0700

    add sdk

--- mylibrary/src/main/java/com/atg/mylibrary/TestLib.kt
+++ mylibrary/src/main/java/com/atg/mylibrary/TestLib.kt
@@ -1,6 +1,8 @@
 package com.atg.mylibrary
 
 import android.util.Log
+import com.visa.cbp.sdk.facade.VisaPaymentSDK
+import com.visa.cbp.sdk.facade.VisaPaymentSDKImpl
 
 object TestLib {
     fun showMessage() {
