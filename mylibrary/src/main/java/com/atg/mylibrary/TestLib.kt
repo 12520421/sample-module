fda1ff62fb9eb1092bb3b15f84ab77a2e02c44de
ThongNguyen <dinhthong.nguyen@alttekglobal.com>
2022-08-31 14:42:35 +0700

    delete import unuse

--- mylibrary/src/main/java/com/atg/mylibrary/TestLib.kt
+++ mylibrary/src/main/java/com/atg/mylibrary/TestLib.kt
@@ -1,9 +1,6 @@
 package com.atg.mylibrary
 
 import android.util.Log
-import com.visa.cbp.sdk.facade.VisaPaymentSDK
-import com.visa.cbp.sdk.facade.VisaPaymentSDKImpl
-
 object TestLib {
     fun showMessage() {
         Log.d("show logg", "show loggg  abc")
